d6827edd29b24faa3f01de6b702caccb4ce630be
Юлия <iulia@MacBook-Pro-Ulia.local>
2024-01-21 14:08:59 +0300

    added function for save

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,50 +5,54 @@ noTask = document.querySelector('.task'),
 newTask = document.querySelector('.new_task'),
 clearButton = document.querySelector('.button');
 
-
-console.log(document.querySelectorAll('li'));
-const saveValue = function(){
-    const arrValue = [];
-    for (let element of newTask.querySelectorAll('li')) {
-        arrValue.push(element.textContent);
-    }
-    localStorage.setItem('task', JSON.stringify(arrValue));
-}
 const createTask = function(){
         const valueTask = inputTask.value;
-
         noTask.classList.add('hide');
 
         const newLi = document.createElement('li');
         newTask.append(newLi);
         inputTask.value = '';
-
         const newLabel = document.createElement('label');
         newLabel.textContent = valueTask;
         newLi.append(newLabel);
-
         const newInput = document.createElement('input');
         newInput.type = 'checkbox';
         newInput.value = valueTask;
         newLabel.append(newInput);
 
+        saveValue();
+
         newInput.addEventListener('click', function () {
             newLi.classList.toggle('completed');
         });
-
         clearButton.disabled = false;
-        saveValue();
 }
 if(inputTask.value !== ''){
     createTask();
 }
-
-
-
-
-
-
+const saveValue = function(){
+    // const arrValue = [];
+    // for (let element of newTask.querySelectorAll('li')) {
+    //     arrValue.push(element.innerHTML);
+    // }JSON.stringify(arrValue)
+    localStorage.setItem('task', newTask.innerHTML);
+}
+function getItem(){
+    // const savedItems = JSON.parse(localStorage.getItem('task'));
+    newTask.innerHTML += localStorage.getItem('task');
+    if (newTask.innerHTML !== ''){
+        noTask.classList.add('hide');
+        clearButton.disabled = false;
+    }
+}
+getItem();
+function clearNewTask() {
+    newTask.innerHTML = '';
+    noTask.classList.remove('hide');
+    clearButton.disabled = false;
+    localStorage.clear();
+}
 
 buttonForm.addEventListener("click", createTask);
-
+clearButton.addEventListener("click", clearNewTask);
 
